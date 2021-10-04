a5d92d8b50270afe42c8d757f15ece586efbaa8f
Clarence-gsx <lonzarude@sjtu.edu.cn>
2021-10-04 18:34:15 -0400

    add provider.py, debug for data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -14,86 +14,89 @@ def main():
     for data in [x for x in os.listdir('./data/ribfrac/ribfrac-train-images-1/Part1/')]:
         source = nib.load('./data/ribfrac/ribfrac-train-images-1/Part1/'+data)
         source = source.get_fdata()
-        source[source != 0] = 1
+        source[source >= 200] = 1
+        source[source != 1] = 0
 
         label = nib.load('./data/RibSeg/nii/'+data[:-12]+'rib-seg.nii.gz')
         label = label.get_fdata()
-        label[label !=0] = 1
 
         temp = np.argwhere(source == 1)
         choice = np.random.choice(temp.shape[0], 30000, replace=False)
         # downsample
         points = temp[choice, :]
 
-        label = []
+        label_selected_points = []
         for i in points:
-            label.append(label[i[0]][i[1]][i[2]])
-        temp = np.array(temp)
+            label_selected_points.append(label[i[0]][i[1]][i[2]])
+        label_selected_points = np.array(label_selected_points)
         np.save('./data/pn/data_pn/train'+data[:-13], points)
-        np.save('./data/pn/label_pn/train' + data[:-13], temp)
+        np.save('./data/pn/label_pn/train' + data[:-13], label_selected_points)
+
 
     for data in [x for x in os.listdir('./data/ribfrac/ribfrac-train-images-2/Part2/')]:
         source = nib.load('./data/ribfrac/ribfrac-train-images-2/Part2/'+data)
         source = source.get_fdata()
-        source[source != 0] = 1
+        source[source >= 200] = 1
+        source[source != 1] = 0
 
         label = nib.load('./data/RibSeg/nii/'+data[:-12]+'rib-seg.nii.gz')
         label = label.get_fdata()
-        label[label !=0] = 1
 
         temp = np.argwhere(source == 1)
         choice = np.random.choice(temp.shape[0], 30000, replace=False)
         # downsample
         points = temp[choice, :]
 
-        label = []
+        label_selected_points = []
         for i in points:
-            label.append(label[i[0]][i[1]][i[2]])
-        temp = np.array(temp)
+            label_selected_points.append(label[i[0]][i[1]][i[2]])
+        label_selected_points = np.array(label_selected_points)
         np.save('./data/pn/data_pn/train'+data[:-13], points)
-        np.save('./data/pn/label_pn/train' + data[:-13], temp)
+        np.save('./data/pn/label_pn/train' + data[:-13], label_selected_points)
+
 
     for data in [x for x in os.listdir('./data/ribfrac/ribfrac-val-images/')]:
         source = nib.load('./ribfrac/ribfrac-val-images/' + data)
         source = source.get_fdata()
-        source[source != 0] = 1
+        source[source >= 200] = 1
+        source[source != 1] = 0
 
         label = nib.load('./data/RibSeg/nii/' + data[:-12] + 'rib-seg.nii.gz')
         label = label.get_fdata()
-        label[label != 0] = 1
 
         temp = np.argwhere(source == 1)
         choice = np.random.choice(temp.shape[0], 30000, replace=False)
         # downsample
         points = temp[choice, :]
 
-        label = []
+        label_selected_points = []
         for i in points:
-            label.append(label[i[0]][i[1]][i[2]])
-        temp = np.array(temp)
+            label_selected_points.append(label[i[0]][i[1]][i[2]])
+        label_selected_points = np.array(label_selected_points)
         np.save('./data/pn/data_pn/val' + data[:-13], points)
-        np.save('./data/pn/label_pn/val' + data[:-13], temp)
+        np.save('./data/pn/label_pn/val' + data[:-13], label_selected_points)
+
 
     for data in [x for x in os.listdir('./data/ribfrac/ribfrac-test-images/')]:
         source = nib.load('./data/ribfrac/ribfrac-test-images/'+data)
         source = source.get_fdata()
-        source[source != 0] = 1
+        source[source >= 200] = 1
+        source[source != 1] = 0
 
         label = nib.load('./data/RibSeg/nii/'+data[:-12]+'rib-seg.nii.gz')
         label = label.get_fdata()
-        label[label !=0] = 1
 
         temp = np.argwhere(source == 1)
         choice = np.random.choice(temp.shape[0], 30000, replace=False)
         # downsample
         points = temp[choice, :]
 
-        label = []
+        label_selected_points = []
         for i in points:
-            label.append(label[i[0]][i[1]][i[2]])
+            label_selected_points.append(label[i[0]][i[1]][i[2]])
         temp = np.array(temp)
         np.save('./data/pn/data_pn/test'+data[:-13], points)
-        np.save('./data/pn/label_pn/test' + data[:-13], temp)
+        np.save('./data/pn/label_pn/test' + data[:-13], label_selected_points)
 
 if __name__ == '__main__':
     main()
